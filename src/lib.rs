38916d893d679195558980a7798485568b5074dd
Aaron Kofsky <aaronko@umich.edu>
2020-12-19 12:14:34 -0500

    Allow parameters for ADSR envelope
    You can now alter the ADSR values via the parameters!

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,13 @@ extern crate vst;
 extern crate rand;
 extern crate wmidi;
 
-use std::convert::TryFrom;
+use std::{convert::TryFrom, iter::Rev, sync::Arc};
 
 use vst::{
     api::{Events, Supported},
     buffer::{AudioBuffer, Outputs},
-    plugin::{CanDo, Category, Info, Plugin},
+    plugin::{CanDo, Category, Info, Plugin, PluginParameters},
+    util::AtomicFloat,
 };
 use wmidi::{MidiMessage, Note, U7};
 
@@ -58,14 +59,14 @@ impl Oscillator {
     }
 
     /// Get num_samples worth of samples from the Oscillator
-    fn values(&mut self, num_samples: usize, sample_rate: f32) -> Vec<f32> {
+    fn values(&mut self, num_samples: usize, sample_rate: f32, envelope: Envelope) -> Vec<f32> {
         let mut buf = Vec::with_capacity(num_samples);
         let mut angle = self.angle;
         let pitch = Note::to_freq_f32(self.pitch);
         for _ in 0..num_samples {
             // Get the sine signal
             let value = (angle * TAU).sin();
-            let value = value * self.envelope(sample_rate);
+            let value = value * self.envelope(sample_rate, envelope);
             buf.push(value);
 
             // Constrain the angle between 0 and 1, reduces roundoff error
@@ -93,49 +94,59 @@ impl Oscillator {
     }
 
     /// Get the current envelope multiplier
-    fn envelope(&self, sample_rate: f32) -> f32 {
-        Oscillator::full_envelope(self.time, self.note_state, sample_rate)
+    fn envelope(&self, sample_rate: f32, envelope: Envelope) -> f32 {
+        Oscillator::full_envelope(self.time, self.note_state, sample_rate, envelope)
     }
 
     /// Release the note
-    fn note_off(&mut self, sample_rate: f32) {
+    fn note_off(&mut self, sample_rate: f32, envelope: Envelope) {
         self.note_state = NoteState::Released {
             time: self.time,
-            vel: self.envelope(sample_rate),
+            vel: self.envelope(sample_rate, envelope),
         };
     }
 
     /// Retrigger the note
-    fn retrigger(&mut self, sample_rate: f32) {
+    fn retrigger(&mut self, sample_rate: f32, envelope: Envelope) {
         self.note_state = NoteState::Retrigger {
             time: self.time,
-            vel: self.envelope(sample_rate),
+            vel: self.envelope(sample_rate, envelope),
         };
     }
 
     /// Returns true if the note is "alive" (playing audio). A note is dead if
     /// it is in the release state and it is after the total release time.
-    fn is_alive(&self, sample_rate: f32) -> bool {
+    fn is_alive(&self, sample_rate: f32, release: f32) -> bool {
         match self.note_state {
             NoteState::Held | NoteState::Retrigger { time: _, vel: _ } => true,
             NoteState::Released {
                 time: release_time,
                 vel: _,
-            } => (self.time - release_time) as f32 / sample_rate < RELEASE,
+            } => (self.time - release_time) as f32 / sample_rate < release,
         }
     }
 
     /// Computes the envelope multiplier. Time is the number of samples since the
     /// start of the note.
-    fn full_envelope(time: usize, note_state: NoteState, sample_rate: f32) -> f32 {
+    fn full_envelope(
+        time: usize,
+        note_state: NoteState,
+        sample_rate: f32,
+        envelope: Envelope,
+    ) -> f32 {
         match note_state {
-            NoteState::Held => ads_env(ATTACK, DECAY, SUSTAIN, time as f32 / sample_rate),
+            NoteState::Held => ads_env(
+                envelope.attack,
+                envelope.decay,
+                envelope.sustain,
+                time as f32 / sample_rate,
+            ),
             NoteState::Released {
                 time: rel_time,
                 vel,
             } => {
                 let time = (time - rel_time) as f32 / sample_rate;
-                lerp(vel, 0.0, time / RELEASE)
+                lerp(vel, 0.0, time / envelope.release)
             }
             NoteState::Retrigger {
                 time: retrig_time,
@@ -149,9 +160,39 @@ impl Oscillator {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Envelope {
+    // In seconds
+    attack: f32,
+    // In seconds
+    decay: f32,
+    // In percent (0.0 to 1.0)
+    sustain: f32,
+    // In seconds
+    release: f32,
+}
+
+impl From<&Arc<RevisitParameters>> for Envelope {
+    fn from(params: &Arc<RevisitParameters>) -> Self {
+        Envelope {
+            attack: params.attack.get(),
+            decay: params.decay.get(),
+            sustain: params.sustain.get(),
+            release: params.release.get(),
+        }
+    }
+}
+struct RevisitParameters {
+    attack: AtomicFloat,
+    decay: AtomicFloat,
+    sustain: AtomicFloat,
+    release: AtomicFloat,
+}
+
 struct Revisit {
     notes: Vec<Oscillator>,
     sample_rate: f32,
+    envelope: Arc<RevisitParameters>,
 }
 
 impl Plugin for Revisit {
@@ -164,7 +205,9 @@ impl Plugin for Revisit {
             unique_id: 413612,
             version: 1,
             category: Category::Synth,
-
+            // 4 parameters -- ADSR
+            parameters: 4,
+            // No audio inputs
             inputs: 0,
             // Two channel audio!
             outputs: 2,
@@ -182,9 +225,12 @@ impl Plugin for Revisit {
 
     // Output audio given the current state of the VST
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        let envelope = Envelope::from(&self.envelope);
+
         // remove "dead" notes
         let sample_rate = self.sample_rate;
-        self.notes.retain(|osc| osc.is_alive(sample_rate));
+        self.notes
+            .retain(|osc| osc.is_alive(sample_rate, envelope.release));
 
         let num_samples = buffer.samples();
         let (_, mut output_buffer) = buffer.split();
@@ -192,7 +238,7 @@ impl Plugin for Revisit {
         let mut output: Vec<f32> = (0..num_samples).map(|_| 0.0).collect();
 
         for note in &mut self.notes {
-            let osc_buffer = note.values(num_samples, self.sample_rate);
+            let osc_buffer = note.values(num_samples, self.sample_rate, envelope);
             output = output
                 .iter()
                 .zip(osc_buffer.iter())
@@ -202,13 +248,14 @@ impl Plugin for Revisit {
 
         for channel in output_buffer.into_iter() {
             for (i, sample) in channel.iter_mut().enumerate() {
-                let volume = 0.25;
+                let volume = 1.0;
                 *sample = output[i] * volume;
             }
         }
     }
 
     fn process_events(&mut self, events: &Events) {
+        let envelope = Envelope::from(&self.envelope);
         for event in events.events() {
             match event {
                 vst::event::Event::Midi(event) => {
@@ -217,14 +264,14 @@ impl Plugin for Revisit {
                         match message {
                             MidiMessage::NoteOn(_, pitch, vel) => {
                                 if let Some(i) = self.notes.iter().position(|x| x.pitch == pitch) {
-                                    self.notes[i].retrigger(self.sample_rate);
+                                    self.notes[i].retrigger(self.sample_rate, envelope);
                                 } else {
                                     self.notes.push(Oscillator::new(pitch, vel));
                                 }
                             }
                             MidiMessage::NoteOff(_, pitch, _) => {
                                 if let Some(i) = self.notes.iter().position(|x| x.pitch == pitch) {
-                                    self.notes[i].note_off(self.sample_rate);
+                                    self.notes[i].note_off(self.sample_rate, envelope);
                                 }
                             }
                             _ => println!("Unrecognized MidiMessage! {:?}", message),
@@ -235,6 +282,70 @@ impl Plugin for Revisit {
             }
         }
     }
+
+    fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
+        Arc::clone(&self.envelope) as Arc<dyn PluginParameters>
+    }
+}
+
+impl PluginParameters for RevisitParameters {
+    fn get_parameter_label(&self, index: i32) -> String {
+        match index {
+            0 | 1 | 3 => " sec".to_string(),
+            2 => "%".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_text(&self, index: i32) -> String {
+        match index {
+            0 => format!("{:.2}", self.attack.get()),
+            1 => format!("{:.2}", self.decay.get()),
+            2 => format!("{:.2}", self.sustain.get() * 100.0),
+            3 => format!("{:.2}", self.release.get()),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_name(&self, index: i32) -> String {
+        match index {
+            0 => "Attack".to_string(),
+            1 => "Decay".to_string(),
+            2 => "Sustain".to_string(),
+            3 => "Release".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter(&self, index: i32) -> f32 {
+        match index {
+            0 => self.attack.get() * 2.0,
+            1 => self.decay.get() * 5.0,
+            2 => self.sustain.get(),
+            3 => self.release.get() * 5.0,
+            _ => 0.0,
+        }
+    }
+
+    fn set_parameter(&self, index: i32, value: f32) {
+        match index {
+            // clamp values to around 1 ms minimum
+            // this avoids division by zero problems
+            0 => self.attack.set((value * 2.0).max(1.0 / 1000.0)),
+            1 => self.decay.set((value * 5.0).max(1.0 / 1000.0)),
+            2 => self.sustain.set(value),
+            3 => self.release.set((value * 5.0).max(1.0 / 1000.0)),
+            _ => (),
+        }
+    }
+
+    fn can_be_automated(&self, index: i32) -> bool {
+        matches!(index, 0 | 1 | 2 | 3)
+    }
+
+    fn string_to_parameter(&self, index: i32, text: String) -> bool {
+        false
+    }
 }
 
 impl Default for Revisit {
@@ -242,6 +353,12 @@ impl Default for Revisit {
         Revisit {
             notes: Vec::with_capacity(16),
             sample_rate: 44100.0,
+            envelope: Arc::new(RevisitParameters {
+                attack: AtomicFloat::new(ATTACK),
+                decay: AtomicFloat::new(DECAY),
+                sustain: AtomicFloat::new(SUSTAIN),
+                release: AtomicFloat::new(RELEASE),
+            }),
         }
     }
 }
@@ -274,6 +391,14 @@ fn ads_env(attack: f32, decay: f32, sustain: f32, time: f32) -> f32 {
 
 fn lerp(start: f32, end: f32, x: f32) -> f32 {
     (end - start) * x.clamp(0.0, 1.0) + start
+}
+
+fn ease_in_expo(x: f32) -> f32 {
+    if x <= 0.0 {
+        0.0
+    } else {
+        2.0f32.powf(10.0 * x - 10.0)
+    }
 }
 
 // Make sure you call this, or nothing will happen.
